bbf09c48e6063fa72310af8639f1fe337887e3e6
Alejandro <aasalazar23@gmail.com>
2020-11-18 07:36:37 -0700

    Add Birthday previewer.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,6 +4,7 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+import BirthdayPagePreview from "./preview-templates/BirthdayPagePreview";
 import ClassCatPreview from "./preview-templates/ClassCatPreview";
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
@@ -14,5 +15,6 @@ CMS.registerMediaLibrary(cloudinary)
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate("birthday", BirthdayPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('classes', ClassCatPreview);
---
+++ src/cms/preview-templates/BirthdayPagePreview.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { BirthdayTemplate } from "../../templates/birthday-page";
+
+const BirthdayPreview = ({ entry, widgetFor, getAsset }) => {
+
+  const entryPoints = entry.getIn(["data", "points"]);
+  const points = entryPoints ? entryPoints.toJS() : [];
+
+  return (
+    <BirthdayTemplate
+      content={widgetFor("body")}
+      title={entry.getIn(["data", "title"])}
+      boxWithLogo={{
+        title: entry.getIn(["data", "boxWithLogo", "title"]),
+        content: entry.getIn(["data", "boxWithLogo", "content"]),
+      }}
+      points={points}
+      textImageBlock={{
+        content: entry.getIn(["data", "textImageBlock", "content"]),
+        image: {
+          image: getAsset(
+            entry.getIn(["data", "textImageBlock", "image", "image"])
+          ),
+          alt: entry.getIn(["data", "textImageBlock", "image", "alt"]),
+        },
+      }}
+    />
+  );
+};
+
+BirthdayPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+  getAsset: PropTypes.func,
+};
+
+export default BirthdayPreview;
