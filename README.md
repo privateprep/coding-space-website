9e428cbf85b9ab3a47518a7b2cf98c8454266547
Dan <danielpowell12@msn.com>
2020-11-20 18:06:02 -0700

    Removes product page
    I'm sure we'll replace this with something similar in due time

--- README.md
+++ README.md
@@ -72,7 +72,6 @@ import CMS from 'netlify-cms-app'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 // CMS.registerMediaLibrary(uploadcare);
@@ -80,7 +79,6 @@ import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 ```
 
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,7 +6,6 @@ import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
 import BirthdayPagePreview from "./preview-templates/BirthdayPagePreview";
 import ClassCatPreview from "./preview-templates/ClassCatPreview";
-import ProductPagePreview from "./preview-templates/ProductPagePreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
 
 CMS.registerMediaLibrary(uploadcare);
@@ -14,7 +13,6 @@ CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate("index", IndexPagePreview);
 CMS.registerPreviewTemplate("about", AboutPagePreview);
-CMS.registerPreviewTemplate("products", ProductPagePreview);
 CMS.registerPreviewTemplate("birthday", BirthdayPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
 CMS.registerPreviewTemplate("classes", ClassCatPreview);
