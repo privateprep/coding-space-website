89a32d76878d91ff8ef56fa32c394330509e7c98
Dan <danielpowell12@msn.com>
2020-11-20 17:22:54 -0700

    Reworks Navbar
    This goes custom and removes the burger. Sure we can add one back in
    when it's time with little effort

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,92 +1,49 @@
 import React from "react";
 import { Link } from "gatsby";
 import logo from "../img/logo.svg";
+import "./Navbar.scss";
 
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false,
-      navBarActiveClass: "",
-    };
-  }
-
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: "is-active",
-            })
-          : this.setState({
-              navBarActiveClass: "",
-            });
-      }
-    );
-  };
-
-  render() {
-    return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
+const Navbar = props => (
+  <nav
+    className="Navbar"
+    role="navigation"
+    aria-label="main-navigation"
+  >
+    <div className="Navbar__container">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item" title="Logo">
+          <img
+            src={logo}
+            alt="The Coding Space Logo"
+            style={{ width: "100%" }}
+          />
+        </Link>
+      </div>
+      <div
+        id="navMenu"
+        className="Navbar__container__menu"
       >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img
-                src={logo}
-                alt="The Coding Space Logo"
-                style={{ width: "100%" }}
-              />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/classes">
-                Classes
-              </Link>
-              <Link className="navbar-item" to="/birthday">
-                Birthday Parties
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-          </div>
-        </div>
-      </nav>
-    );
-  }
-};
+        <Link className="navbar-item" to="/classes">
+          Classes
+        </Link>
+        <Link className="navbar-item" to="/birthday">
+          Birthday Parties
+        </Link>
+        <Link className="navbar-item" to="/blog">
+          Blog
+        </Link>
+        <Link className="navbar-item" to="/about">
+          About
+        </Link>
+        <Link className="navbar-item" to="/contact">
+          Contact
+        </Link>
+        <Link className="navbar-item" to="/contact/examples">
+          Forms
+        </Link>
+      </div>
+    </div>
+  </nav>
+);
 
 export default Navbar;
