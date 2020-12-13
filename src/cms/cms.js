327e7b0d324266d499337fb6fca32e5696e579a0
Alejandro <aasalazar23@gmail.com>
2020-12-12 17:24:28 -0700

    Add a color picker with company colors.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,7 @@
 import CMS from "netlify-cms-app";
 import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
+import { ColorControl, ColorPreview } from "netlify-cms-widget-colorpicker";
 
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
@@ -16,3 +17,7 @@ CMS.registerPreviewTemplate("about", AboutPagePreview);
 CMS.registerPreviewTemplate("birthday", BirthdayPagePreview);
 CMS.registerPreviewTemplate("blog", BlogPostPreview);
 CMS.registerPreviewTemplate("classes", ClassCatPreview);
+
+
+// Widgets
+CMS.registerWidget("color", ColorControl, ColorPreview);
