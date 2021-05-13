a0c5d169a822e183d27a389ff34a713325970350
Alejandro <aasalazar23@gmail.com>
2021-05-13 15:14:25 -0600

    Passes props to customLinkBox.

--- src/components/BuilderComponents/CustomLinkBoxes.js
+++ src/components/BuilderComponents/CustomLinkBoxes.js
@@ -3,20 +3,27 @@ import PropTypes from "prop-types";
 import createHtml from "../MdToHtml";
 
 const CustomLinkBox = ({
-  fgColor,
+  boxColor,
   mdContent,
-  content,
+  link,
   textAlign,
   textColor,
 }) => {
   const htmlContent = createHtml(mdContent);
+
   return (
     <li
       className="custom-link-box"
-      style={{ backgroundColor: fgColor, color: textColor }}
+      style={{
+        backgroundColor: boxColor,
+        color: textColor,
+        border: `solid 2px ${textColor}`,
+        borderRadius: "5px",
+      }}
     >
-      <a href={content}>
+      <a className="custom-link-box__link" href={link}>
         <div
+          className="custom-link-box__content"
           dangerouslySetInnerHTML={htmlContent}
           style={{ color: textColor, textAlign: textAlign }}
         />
@@ -37,8 +44,11 @@ const CustomLinkBoxes = ({ data }) => {
           list.map((item, i) => (
             <CustomLinkBox
               key={`custom-link-box-${i}`}
-              link={item.link}
+              link={item.content}
               mdContent={item.mdContent}
+              boxColor={item.fgColor}
+              textAlign={item.textAlign}
+              textColor={item.textColor}
             />
           ))}
       </ul>
