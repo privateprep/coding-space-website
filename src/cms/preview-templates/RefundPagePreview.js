f9ef2508f53f288c8d6b253abfef5a0a81c16198
Alejandro <aasalazar23@gmail.com>
2021-06-01 16:57:01 -0600

    Remove debugger, destructure entry.

--- src/cms/preview-templates/RefundPagePreview.js
+++ src/cms/preview-templates/RefundPagePreview.js
@@ -2,11 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import { RefundPageTemplate } from "../../templates/Refund-page";
 
-const RefundPagePreview = (props) => {
-  const { entry } = props;
+const RefundPagePreview = ({ entry }) => {
   const entryPageBuilder = entry.getIn(["data", "pageBuilder"]);
   const pageBuilder = entryPageBuilder ? entryPageBuilder.toJS() : [];
-  debugger;
+
   return (
     <RefundPageTemplate
       title={entry.getIn(["data", "title"])}
