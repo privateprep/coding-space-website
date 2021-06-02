adee06eec3cef8bac6bf6262e05f09cc49b23d79
Dan <danielpowell12@msn.com>
2021-06-02 07:49:37 -0600

    Adds basic header
    This will evolve come launch time

---
+++ src/components/BasicHeader.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link, graphql, useStaticQuery } from "gatsby";
+
+import logo from "../img/logo.svg";
+
+// NOTE: ./NavBar is a bit more built out!
+
+const BasicHeader = () => {
+  const homeLink =
+    process.env.NODE_ENV === "production" ? "https://thecodingspace.com" : "/";
+
+  return (
+    <header
+      style={{
+        background: `#fff`,
+      }}
+    >
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 1170,
+          padding: `1.45rem 1.0875rem`,
+          display: `flex`,
+          flexFlow: `row wrap`,
+          justifyContent: `space-between`,
+          alignItems: `center`,
+        }}
+      >
+        <a
+          href={homeLink}
+          style={{
+            color: `#274548`,
+            textDecoration: `none`,
+            display: `block`,
+          }}
+        >
+          <img
+            src={logo}
+            alt="The Coding Space"
+            style={{
+              width: 260,
+              fill: `#274548`,
+              marginRight: `2rem`,
+            }}
+          />
+        </a>
+        <div>
+          <Link
+            to={"/locations"}
+            style={{ color: `var(--dark)`, marginLeft: `1rem` }}
+          >
+            Locations
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default BasicHeader;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,84 +1,85 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import BasicHeader from "../components/BasicHeader";
 import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
 import "./all.scss";
 import "../fonts/fonts.css";
 import useSiteMetadata from "./SiteMetadata";
 import { withPrefix } from "gatsby";
 
-const menuItems = [
-  {
-    title: "Get Started - WIP",
-    subMenu: [
-      { title: "Help me decide?", path: "/whats-new" },
-      { title: "What's New", path: "/whats-new" },
-      { title: "Semester Classes", path: "/semester-classes" },
-      { title: "Girlcode", path: "signup/girlcode" },
-      { title: "Camps", path: "/camps" },
-    ],
-  },
-  {
-    title: "Programs",
-    path: "",
-    subMenu: [
-      { title: "Experience Levels", path: "/programs/experience-levels" },
-      {
-        title: "Seasonal Programs",
-        path: "/programs/seasonal-programs",
-      },
-      {
-        title: "Experiential Programs",
-        path: "/programs/experiential-programs",
-      },
-      {
-        title: "Camps",
-        path: "/programs/camps",
-      },
-      {
-        title: "Build Your Own",
-        path: "/programs/build-your-own",
-      },
-      {
-        title: "Special Events",
-        path: "/programs/special-events",
-      },
-    ],
-  },
-  {
-    title: "Partnerships",
-    path: "",
-    subMenu: [
-      { title: "Schools", path: "partnerships/schools" },
-      { title: "Community", path: "partnerships/community" },
-      { title: "Corporate", path: "partnerships/corporate" },
-      { title: "Camps", path: "partnerships/camps" },
-    ],
-  },
-  {
-    title: "About Us",
-    path: "",
-    subMenu: [
-      { title: "How TCS is different", path: "about-us/how-tcs-is-different" },
-      { title: "Social Impact", path: "about-us/social-impact" },
-      { title: "Common Questions", path: "about-us/common-questions" },
-      { title: "School Programs", path: "about-us/school-programs" },
-      { title: "Rewards Program", path: "about-us/rewards-program" },
-      { title: "Our Philosophy", path: "about-us/out-philosophy" },
-      { title: "LMS", path: "/lms" },
-    ],
-  },
-  {
-    title: "Blogs",
-    path: "/blogs",
-    subMenu: [],
-  },
-  {
-    title: "Contact Us",
-    path: "/contact",
-    subMenu: [],
-  },
-];
+// const menuItems = [
+//   {
+//     title: "Get Started - WIP",
+//     subMenu: [
+//       { title: "Help me decide?", path: "/whats-new" },
+//       { title: "What's New", path: "/whats-new" },
+//       { title: "Semester Classes", path: "/semester-classes" },
+//       { title: "Girlcode", path: "signup/girlcode" },
+//       { title: "Camps", path: "/camps" },
+//     ],
+//   },
+//   {
+//     title: "Programs",
+//     path: "",
+//     subMenu: [
+//       { title: "Experience Levels", path: "/programs/experience-levels" },
+//       {
+//         title: "Seasonal Programs",
+//         path: "/programs/seasonal-programs",
+//       },
+//       {
+//         title: "Experiential Programs",
+//         path: "/programs/experiential-programs",
+//       },
+//       {
+//         title: "Camps",
+//         path: "/programs/camps",
+//       },
+//       {
+//         title: "Build Your Own",
+//         path: "/programs/build-your-own",
+//       },
+//       {
+//         title: "Special Events",
+//         path: "/programs/special-events",
+//       },
+//     ],
+//   },
+//   {
+//     title: "Partnerships",
+//     path: "",
+//     subMenu: [
+//       { title: "Schools", path: "partnerships/schools" },
+//       { title: "Community", path: "partnerships/community" },
+//       { title: "Corporate", path: "partnerships/corporate" },
+//       { title: "Camps", path: "partnerships/camps" },
+//     ],
+//   },
+//   {
+//     title: "About Us",
+//     path: "",
+//     subMenu: [
+//       { title: "How TCS is different", path: "about-us/how-tcs-is-different" },
+//       { title: "Social Impact", path: "about-us/social-impact" },
+//       { title: "Common Questions", path: "about-us/common-questions" },
+//       { title: "School Programs", path: "about-us/school-programs" },
+//       { title: "Rewards Program", path: "about-us/rewards-program" },
+//       { title: "Our Philosophy", path: "about-us/out-philosophy" },
+//       { title: "LMS", path: "/lms" },
+//     ],
+//   },
+//   {
+//     title: "Blogs",
+//     path: "/blogs",
+//     subMenu: [],
+//   },
+//   {
+//     title: "Contact Us",
+//     path: "/contact",
+//     subMenu: [],
+//   },
+// ];
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata();
@@ -123,7 +124,7 @@ const TemplateWrapper = ({ children }) => {
         />
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Helmet>
-      <Navbar menuItems={menuItems} />
+      <BasicHeader />
       <div className="page-container">{children}</div>
       <Footer />
     </div>
