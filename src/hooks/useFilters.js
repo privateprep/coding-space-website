3d02d6ad01002a885c5da53856eb709a1d9e894d
Dan <danielpowell12@msn.com>
2021-06-04 16:53:21 -0600

    Ensures options sorted

--- src/hooks/useFilters.js
+++ src/hooks/useFilters.js
@@ -72,7 +72,21 @@ const buildOptions = (collection, filter) => {
     }
   }
 
-  return options;
+  if (filter.filterKey === "class_location_ids") {
+    return options.sort((a, b) => {
+      /* online goes last */
+      const aIsOnline = a.label.toLowerCase().includes("online");
+      const bIsOnline = b.label.toLowerCase().includes("online");
+      if (aIsOnline !== bIsOnline) {
+        if (aIsOnline) return 1;
+        if (bIsOnline) return -1;
+      }
+
+      return a.value.localeCompare(b.value);
+    })
+  } else {
+    return options.sort((a, b) => a.value.localeCompare(b.value));
+  }
 };
 
 // builds initial filter state
