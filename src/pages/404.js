242f79b94face731a40717298140af92a996a9a5
Dan <danielpowell12@msn.com>
2021-06-07 14:51:23 -0600

    Adds simple 404
    This is nothing glorious but feels at tid bit better than the previous
    default

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { Link } from "gatsby";
 import Layout from "../components/Layout";
 
 const NotFoundPage = () => (
   <Layout>
-    <div>
+    <div style={{ width: "100%", maxWidth: 760, padding: "3rem 1rem 6rem", margin: "auto" }}>
       <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>Oh no! You landed on a page that does not exist or has moved to a new place.</p>
+      <p>To find for a class, check out our <Link to={`/classes`}>class catalog</Link> or <Link to={`/locations`}>locations</Link>!</p>
+      <p>If you'd prefer, you can always <a href="//thecodingspace.com/contact.html">contact our team</a> to speak to a human.</p>
     </div>
+    <hr />
   </Layout>
-);
+)
 
 export default NotFoundPage;
