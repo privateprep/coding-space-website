0890d99981705b900cb805bfd9cd820d321ae063
Alejandro <aasalazar23@gmail.com>
2021-10-19 09:37:12 -0600

    Add initial AccordionItem component.
    Inspired by the clean look found here
    https://github.com/ALapina/FAQ-Accordion-Card-React.

---
+++ src/components/Atoms/AccordionItem.js
@@ -0,0 +1,71 @@
+import React from "react";
+import PropTypes from "prop-types";
+import createHtml from "../MdToHtml";
+
+import "../BuilderComponents/styles/Accordion.scss";
+
+export const AccordionItem = ({
+  activeItem,
+  index,
+  mdContent,
+  onClick,
+  textColor,
+  title,
+}) => {
+  const htmlContent = createHtml(mdContent);
+
+  return (
+    <div
+      className={`accordion-item ${!!activeItem ? "display-content" : ""}`}
+      key={`accordion-item${index}`}
+    >
+      <dt>
+        <button
+          aria-expanded={activeItem}
+          aria-controls={`accordion-item${index}_desc`}
+          className={`accordion-item__button ${
+            !!activeItem ? "display-content" : ""
+          }`}
+          style={{ color: textColor }}
+          onClick={onClick}
+        >
+          {title}
+        </button>
+      </dt>
+      <dd>
+        <div
+          id={`accordion-item${index}_desc`}
+          className={`accordion-item__desc ${
+            !!activeItem ? "display-content" : ""
+          }`}
+          dangerouslySetInnerHTML={htmlContent}
+        />
+      </dd>
+    </div>
+  );
+};
+
+AccordionItem.propTypes = {
+  /**
+   Indicate if the item is expanded or not.
+  */
+  activeItem: PropTypes.bool,
+  /**
+   Used as key prop to identify the item.
+  */
+  index: PropTypes.number,
+  /**
+   Rich content display in the accordion item after clicking on the title.
+  */
+  mdContent: PropTypes.string,
+  /**
+    Callback function to be executed when the title is clicked in order to set display.
+  */
+  onClick: PropTypes.func,
+  /**
+   The title of the accordion item, always displayed.
+  */
+  title: PropTypes.string,
+};
+
+export default AccordionItem;
