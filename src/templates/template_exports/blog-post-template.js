9bdf6639a3e12564506a5edaf543843a4d6e46c2
Alejandro <aasalazar23@gmail.com>
2021-11-16 15:44:12 -0700

    Significant styling adjustments.
    Moves tags to top of the page, outside of the blog post.
    - Adds proper spacing between details, header, and content.
    - Limits image sizes in content
    - Limit featured image to 35vh to improve visibility of text.

--- src/templates/template_exports/blog-post-template.js
+++ src/templates/template_exports/blog-post-template.js
@@ -22,26 +22,30 @@ export const BlogPostTemplate = ({
     <React.Fragment>
       {helmet || ""}
       <div className="blog-container" style={{ backgroundColor: bgColor }}>
+        {tags && tags.length ? (
+          <div className="tags">
+            <ul className="taglist">
+              {tags.map((tag) => (
+                <li key={tag + `tag`}>
+                  <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : null}
         <div className="blog-post">
-          <div className="blog-post__info">
+          <div className="blog-post__details">
             <span>{date}</span>
           </div>
-          <h1 style={{ color: titleColor }}>{title}</h1>
-          <PreviewCompatibleImage imageInfo={featuredImage} />
-          <p>{description}</p>
-          <PostContent content={content} />
-          {tags && tags.length ? (
-            <div style={{ marginTop: `4rem` }}>
-              <h4>Tags</h4>
-              <ul className="taglist">
-                {tags.map((tag) => (
-                  <li key={tag + `tag`}>
-                    <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ) : null}
+          <div className="blog-post__heading">
+            <h1 style={{ color: titleColor }}>{title}</h1>
+            <PreviewCompatibleImage imageInfo={featuredImage} />
+            <h2>{description}</h2>
+          </div>
+          <div className="blog-post__content">
+            <PostContent content={content} />
+          </div>
+          <hr />
         </div>
       </div>
     </React.Fragment>
