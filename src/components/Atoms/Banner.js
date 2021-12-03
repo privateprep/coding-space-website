f1e4f9dc3af57538c01931d9ae165a2af22612ab
Alejandro <aasalazar23@gmail.com>
2021-12-03 14:47:19 -0700

    Add Banner component.
    This will allow for richtext editing of the banner to support links.
    Additionally the banner will self remove itself if the removeDate is
    after the current time. This will minimize the forgotten outdated banners
    we have from time to time.

---
+++ src/components/Atoms/Banner.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import createHtml from "../MdToHtml";
+import { DateTime } from "luxon";
+
+import "./styles/Banner.scss";
+
+const Banner = ({ mdContent, removalDate }) => {
+  const htmlContent = createHtml(mdContent);
+  const currentDate = DateTime.now().setZone("America/New_York");
+  const removalDateTime =
+    DateTime.fromISO(removalDate).setZone("America/New_York");
+
+  if (!!removalDate && currentDate > removalDateTime) {
+    return null;
+  }
+
+  return (
+    <div className="Banner component">
+      <div dangerouslySetInnerHTML={htmlContent} />
+    </div>
+  );
+};
+
+Banner.propTypes = {
+  /**
+   * The datetime that the banner will be removed from the page
+   */
+  removalDate: PropTypes.string,
+  /**
+   * Rich text to allow links
+   */
+  mdContent: PropTypes.string,
+};
+
+export default Banner;
