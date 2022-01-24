fefbaeb5ba36fbfeb45628021f21f9e4f3d499a1
Alejandro <aasalazar23@gmail.com>
2022-01-24 16:27:25 -0700

    Remove extra container around pages.

--- src/templates/template_exports/about-us-template.js
+++ src/templates/template_exports/about-us-template.js
@@ -17,30 +17,28 @@ export const AboutUsTemplate = ({
   return (
     <section className="section">
       {helmet || ""}
-      <div className="container content">
-        <div
-          className="full-width-image margin-top-0"
-          style={{
-            backgroundImage: `url(${backgroundImage})`,
-            backgroundPosition: "left",
-            width: "100%",
-          }}
-        >
-          <div className="columns">
-            <div className="column">
-              <h1
-                className="title"
-                style={{ color: titleColor, fontSize: "5em" }}
-              >
-                {title}
-              </h1>
-            </div>
+      <div
+        className="full-width-image margin-top-0"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundPosition: "left",
+          width: "100%",
+        }}
+      >
+        <div className="columns">
+          <div className="column">
+            <h1
+              className="title"
+              style={{ color: titleColor, fontSize: "5em" }}
+            >
+              {title}
+            </h1>
           </div>
         </div>
+      </div>
 
-        <div>
-          <PageBuilder data={data} />
-        </div>
+      <div>
+        <PageBuilder data={data} />
       </div>
     </section>
   );
