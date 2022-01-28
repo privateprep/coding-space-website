64d194e2e69202366718886789be27e616133eb2
Alejandro <aasalazar23@gmail.com>
2022-01-28 12:16:27 -0700

    Spruce up contact-us thank you page.

--- src/pages/contact-us/thanks.js
+++ src/pages/contact-us/thanks.js
@@ -1,17 +1,18 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import BoxWithLogo from "../../components/BuilderComponents/BoxWithLogo";
 
-const Thanks = () => (
-  <Layout>
-    <section className="section">
-      <div className="container">
-        <div className="content">
-          <h1>Thank you!</h1>
-          <p>We'll be in touch</p>
-        </div>
-      </div>
-    </section>
-  </Layout>
-);
+const Thanks = () => {
+  const heading = "We'll be in touch!";
+  const mdContent =
+    "You've successfully inquired about The Coding Space! We're excited to help your child started on their coding journey!";
+  return (
+    <Layout>
+      <section className="newsletter-success">
+        <BoxWithLogo heading={heading} mdContent={mdContent} />
+      </section>
+    </Layout>
+  );
+};
 
 export default Thanks;
